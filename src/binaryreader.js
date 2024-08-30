09346f745afc0a66836d27061d8aa29a7ebebf12
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-08-30 18:12:30 +0200

    Added Private properties

--- src/binaryreader.js
+++ src/binaryreader.js
@@ -11,69 +11,72 @@ export function getCharCodes(string) {
 }
 
 export class BinaryReader {
+	#dataView;
+	#byteOffset = 0;
+	#littleEndian;
 	constructor(buffer, byteOffset, byteLength, littleEndian = true) {
-		this.byteOffset = 0;
-		this.littleEndian = littleEndian;
+		this.#byteOffset = 0;
+		this.#littleEndian = littleEndian;
 		this.#initDataview(buffer, byteOffset, byteLength);
 	}
 
 	#initDataview(buffer, byteOffset, byteLength) {
 		if (buffer instanceof BinaryReader) {
-			this._dataView = new DataView(buffer.buffer, byteOffset ? byteOffset + buffer._dataView.byteOffset : buffer._dataView.byteOffset, byteLength);
+			this.#dataView = new DataView(buffer.buffer, byteOffset ? byteOffset + buffer.#dataView.byteOffset : buffer.#dataView.byteOffset, byteLength);
 		} else if (buffer instanceof Uint8Array || buffer?.constructor?.name === 'Uint8Array') {
-			this._dataView = new DataView(buffer.buffer, byteOffset ? byteOffset + buffer.byteOffset : buffer.byteOffset, byteLength);
+			this.#dataView = new DataView(buffer.buffer, byteOffset ? byteOffset + buffer.byteOffset : buffer.byteOffset, byteLength);
 		} else if (buffer instanceof ArrayBuffer || buffer?.constructor?.name === 'ArrayBuffer') {
-			this._dataView = new DataView(buffer, byteOffset, byteLength);
+			this.#dataView = new DataView(buffer, byteOffset, byteLength);
 		} else if (typeof buffer === 'string') {
-			this._dataView = new DataView(getCharCodes(buffer).buffer, byteOffset, byteLength);
+			this.#dataView = new DataView(getCharCodes(buffer).buffer, byteOffset, byteLength);
 		} else if (typeof buffer === 'number') {
-			this._dataView = new DataView(new Uint8Array(buffer).buffer, byteOffset, byteLength);
+			this.#dataView = new DataView(new Uint8Array(buffer).buffer, byteOffset, byteLength);
 		} else {
 			console.error(`Unknow buffer type : ${buffer}`);
 		}
 	}
 
 	get buffer() {
-		return this._dataView.buffer;
+		return this.#dataView.buffer;
 	}
 
 	get byteLength() {
-		return this._dataView.byteLength;
+		return this.#dataView.byteLength;
 	}
 
 	tell() {
-		return this.byteOffset;
+		return this.#byteOffset;
 	}
 
-	seek(byteOffset = this.byteOffset) {
+	seek(byteOffset = this.#byteOffset) {
 		// /_checkBounds
-		this.byteOffset = byteOffset;
+		this.#byteOffset = byteOffset;
 	}
 
 	skip(byteLength = 0) {
 		// /_checkBounds
-		this.byteOffset += byteLength;
+		this.#byteOffset += byteLength;
 	}
 
-	getString(byteLength, byteOffset = this.byteOffset) {
+	getString(byteLength, byteOffset = this.#byteOffset) {
 		let string = '';
-		let readBuffer = new Uint8Array(this.buffer, byteOffset + this._dataView.byteOffset, byteLength);
+		let readBuffer = new Uint8Array(this.buffer, byteOffset + this.#dataView.byteOffset, byteLength);
 		// /_checkBounds
-		this.byteOffset = byteOffset + byteLength;
+		this.#byteOffset = byteOffset + byteLength;
 		for (var i = 0; i < byteLength; i++) {
 			string += String.fromCharCode(readBuffer[i]);
 		}
 		return string;
 	}
 
-	getNullString(byteOffset = this.byteOffset) {
+	getNullString(byteOffset = this.#byteOffset) {
 		let string = '';
-		let readBuffer = new Uint8Array(this.buffer, this._dataView.byteOffset);
+		let readBuffer = new Uint8Array(this.buffer, this.#dataView.byteOffset);
 
-		this.byteOffset = byteOffset;
+		this.#byteOffset = byteOffset;
 		let c;
 		do {
-			c = String.fromCharCode(readBuffer[this.byteOffset++]);
+			c = String.fromCharCode(readBuffer[this.#byteOffset++]);
 			if (c == '\0') {
 			} else {
 				string += c;
@@ -82,10 +85,10 @@ export class BinaryReader {
 		return string;
 	}
 
-	setString(string, byteOffset = this.byteOffset) {
+	setString(string, byteOffset = this.#byteOffset) {
 		let length = string.length;
-		this.byteOffset = byteOffset + length;
-		let writeBuffer = new Uint8Array(this.buffer, byteOffset + this._dataView.byteOffset, length);
+		this.#byteOffset = byteOffset + length;
+		let writeBuffer = new Uint8Array(this.buffer, byteOffset + this.#dataView.byteOffset, length);
 		//TODO: check len
 
 		for (var i = 0, l = length; i < l; i++) {
@@ -93,37 +96,37 @@ export class BinaryReader {
 		}
 	}
 
-	getBytes(byteLength, byteOffset = this.byteOffset) {
-		let readBuffer = new Uint8Array(this.buffer, byteOffset + this._dataView.byteOffset, byteLength);
-		this.byteOffset = byteOffset + byteLength;
+	getBytes(byteLength, byteOffset = this.#byteOffset) {
+		let readBuffer = new Uint8Array(this.buffer, byteOffset + this.#dataView.byteOffset, byteLength);
+		this.#byteOffset = byteOffset + byteLength;
 		return readBuffer;
 	}
 
-	getInt8(byteOffset = this.byteOffset) {
-		this.byteOffset = byteOffset + 1;
-		return this._dataView.getInt8(byteOffset);
+	getInt8(byteOffset = this.#byteOffset) {
+		this.#byteOffset = byteOffset + 1;
+		return this.#dataView.getInt8(byteOffset);
 	}
 
-	getUint8(byteOffset = this.byteOffset) {
-		this.byteOffset = byteOffset + 1;
-		return this._dataView.getUint8(byteOffset);
+	getUint8(byteOffset = this.#byteOffset) {
+		this.#byteOffset = byteOffset + 1;
+		return this.#dataView.getUint8(byteOffset);
 	}
 
-	getInt16(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 2;
-		return this._dataView.getInt16(byteOffset, littleEndian);
+	getInt16(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 2;
+		return this.#dataView.getInt16(byteOffset, littleEndian);
 	}
 
-	getUint16(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 2;
-		return this._dataView.getUint16(byteOffset, littleEndian);
+	getUint16(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 2;
+		return this.#dataView.getUint16(byteOffset, littleEndian);
 	}
 
-	getFloat16(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {//TODOv3: optimize this function
+	getFloat16(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {//TODOv3: optimize this function
 		//TODO: fix endianness
-		this.byteOffset = byteOffset + 2;
+		this.#byteOffset = byteOffset + 2;
 
-		let readBuffer = new Uint8Array(this.buffer, byteOffset + this._dataView.byteOffset, 2);//TODOv3: optimize
+		let readBuffer = new Uint8Array(this.buffer, byteOffset + this.#dataView.byteOffset, 2);//TODOv3: optimize
 		let b = readBuffer;//this._getBytes(2, byteOffset, littleEndian);
 
 		let sign = b[1] >> 7;
@@ -139,44 +142,44 @@ export class BinaryReader {
 		return (sign ? -1 : 1) * Math.pow(2, exponent - 15) * (1 + (mantissa / TWO_POW_10));
 	}
 
-	getInt32(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 4;
-		return this._dataView.getInt32(byteOffset, littleEndian);
+	getInt32(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 4;
+		return this.#dataView.getInt32(byteOffset, littleEndian);
 	}
 
-	getUint32(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 4;
-		return this._dataView.getUint32(byteOffset, littleEndian);
+	getUint32(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 4;
+		return this.#dataView.getUint32(byteOffset, littleEndian);
 	}
 
-	getFloat32(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 4;
-		return this._dataView.getFloat32(byteOffset, littleEndian);
+	getFloat32(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 4;
+		return this.#dataView.getFloat32(byteOffset, littleEndian);
 	}
 
-	getBigInt64(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 8;
-		return this._dataView.getBigInt64(byteOffset, littleEndian);
+	getBigInt64(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 8;
+		return this.#dataView.getBigInt64(byteOffset, littleEndian);
 	}
 
-	getBigUint64(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 8;
-		return this._dataView.getBigUint64(byteOffset, littleEndian);
+	getBigUint64(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 8;
+		return this.#dataView.getBigUint64(byteOffset, littleEndian);
 	}
 
-	getFloat64(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 8;
-		return this._dataView.getFloat64(byteOffset, littleEndian);
+	getFloat64(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 8;
+		return this.#dataView.getFloat64(byteOffset, littleEndian);
 	}
 
-	getVector2(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
+	getVector2(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
 		let vec = new Float32Array(2);
 		vec[0] = this.getFloat32(byteOffset, littleEndian);
 		vec[1] = this.getFloat32(undefined, littleEndian);
 		return vec;
 	}
 
-	getVector3(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
+	getVector3(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
 		let vec = new Float32Array(3);
 		vec[0] = this.getFloat32(byteOffset, littleEndian);
 		vec[1] = this.getFloat32(undefined, littleEndian);
@@ -184,7 +187,7 @@ export class BinaryReader {
 		return vec;
 	}
 
-	getVector4(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
+	getVector4(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
 		let vec = new Float32Array(4);
 		vec[0] = this.getFloat32(byteOffset, littleEndian);
 		vec[1] = this.getFloat32(undefined, littleEndian);
@@ -193,7 +196,7 @@ export class BinaryReader {
 		return vec;
 	}
 
-	getVector48(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
+	getVector48(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
 		let vec = new Float32Array(3);
 		vec[0] = this.getFloat16(byteOffset, littleEndian);
 		vec[1] = this.getFloat16(undefined, littleEndian);
@@ -201,7 +204,7 @@ export class BinaryReader {
 		return vec;
 	}
 
-	getQuaternion(byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
+	getQuaternion(byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
 		let vec = new Float32Array(4);
 		vec[0] = this.getFloat32(byteOffset, littleEndian);
 		vec[1] = this.getFloat32(undefined, littleEndian);
@@ -210,59 +213,59 @@ export class BinaryReader {
 		return vec;
 	}
 
-	setBigInt64(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 8;
-		return this._dataView.setBigInt64(byteOffset, value, littleEndian);
+	setBigInt64(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 8;
+		return this.#dataView.setBigInt64(byteOffset, value, littleEndian);
 	}
 
-	setBigUint64(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 8;
-		return this._dataView.setBigUint64(byteOffset, value, littleEndian);
+	setBigUint64(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 8;
+		return this.#dataView.setBigUint64(byteOffset, value, littleEndian);
 	}
 
-	setFloat32(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 4;
-		return this._dataView.setFloat32(byteOffset, value, littleEndian);
+	setFloat32(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 4;
+		return this.#dataView.setFloat32(byteOffset, value, littleEndian);
 	}
 
-	setFloat64(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 8;
-		return this._dataView.setFloat64(byteOffset, value, littleEndian);
+	setFloat64(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 8;
+		return this.#dataView.setFloat64(byteOffset, value, littleEndian);
 	}
 
-	setInt8(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 1;
-		return this._dataView.setInt8(byteOffset, value, littleEndian);
+	setInt8(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 1;
+		return this.#dataView.setInt8(byteOffset, value, littleEndian);
 	}
 
-	setInt16(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 2;
-		return this._dataView.setInt16(byteOffset, value, littleEndian);
+	setInt16(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 2;
+		return this.#dataView.setInt16(byteOffset, value, littleEndian);
 	}
 
-	setInt32(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 4;
-		return this._dataView.setInt32(byteOffset, value, littleEndian);
+	setInt32(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 4;
+		return this.#dataView.setInt32(byteOffset, value, littleEndian);
 	}
 
-	setUint8(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 1;
-		return this._dataView.setUint8(byteOffset, value, littleEndian);
+	setUint8(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 1;
+		return this.#dataView.setUint8(byteOffset, value, littleEndian);
 	}
 
-	setUint16(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 2;
-		return this._dataView.setUint16(byteOffset, value, littleEndian);
+	setUint16(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 2;
+		return this.#dataView.setUint16(byteOffset, value, littleEndian);
 	}
 
-	setUint32(value, byteOffset = this.byteOffset, littleEndian = this.littleEndian) {
-		this.byteOffset = byteOffset + 4;
-		return this._dataView.setUint32(byteOffset, value, littleEndian);
+	setUint32(value, byteOffset = this.#byteOffset, littleEndian = this.#littleEndian) {
+		this.#byteOffset = byteOffset + 4;
+		return this.#dataView.setUint32(byteOffset, value, littleEndian);
 	}
 
-	setBytes(bytes, byteOffset = this.byteOffset) {
+	setBytes(bytes, byteOffset = this.#byteOffset) {
 		let length = bytes.length;
-		this.byteOffset = byteOffset + length;
-		new Uint8Array(this._dataView.buffer, byteOffset + this._dataView.byteOffset, length).set(bytes);
+		this.#byteOffset = byteOffset + length;
+		new Uint8Array(this.#dataView.buffer, byteOffset + this.#dataView.byteOffset, length).set(bytes);
 	}
 }
